42cc56fac67abc5cadcf708fd6e37c693b5f958b
coslush <98059055+coslush@users.noreply.github.com>
2025-09-04 12:24:58 -0400

    Update GetBearerToken.py

--- orchestrator/workflows/GetBearerToken.py
+++ orchestrator/workflows/GetBearerToken.py
@@ -1,3 +1,16 @@
+#    Name: GetBearerToken
+#
+#    Inputs:
+#        None
+#
+#    Outputs:
+#        bearerToken : string
+#
+#    Note: This should probably be changed to an Action
+#
+#    Source: 
+#       https://cloudblogger.co.in/2024/04/10/simplest-way-to-generate-bearer-token-in-orchestrator-for-aria-automation-cb10135/
+#
 import json
 def handler(context, inputs):
     outputs = inputs
